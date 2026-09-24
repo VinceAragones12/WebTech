dac1e0cf390d4c2698441e5fca2c03ad25d114fe
VinceAragones12 <jairusvince123@gmail.com>
2026-09-24 09:18:32 +0800

    Add files via upload

---
+++ WebTech/schedules.c
@@ -0,0 +1,72 @@
+<table border="1">
+    <tr>
+        <th colspan="5">Weekly Class Schedule</th>
+    </tr>
+
+    <tr>
+        <th>Day</th>
+        <th>Time</th>
+        <th>Subject</th>
+        <th>Room</th>
+        <th>Instructor</th>
+    </tr>
+
+    <tr>
+        <td rowspan="2">Monday</td>
+        <td>8:00 AM - 10:00 AM</td>
+        <td>Web Development</td>
+        <td>LAB 1</td>
+        <td>Mr. Santos</td>
+    </tr>
+
+    <tr>
+        <td>10:00 AM - 12:00 PM</td>
+        <td>Database Systems</td>
+        <td>LAB 2</td>
+        <td>Ms. Cruz</td>
+    </tr>
+
+    <tr>
+        <td rowspan="2">Tuesday</td>
+        <td>8:00 AM - 10:00 AM</td>
+        <td>Programming</td>
+        <td>LAB 3</td>
+        <td>Mr. Reyes</td>
+    </tr>
+
+    <tr>
+        <td>1:00 PM - 3:00 PM</td>
+        <td>Networking</td>
+        <td>ROOM 204</td>
+        <td>Ms. Garcia</td>
+    </tr>
+
+    <tr>
+        <td>Wednesday</td>
+        <td colspan="2">9:00 AM - 12:00 PM<br>System Analysis</td>
+        <td>ROOM 301</td>
+        <td>Mr. Dela Cruz</td>
+    </tr>
+
+    <tr>
+        <td rowspan="2">Thursday</td>
+        <td>8:00 AM - 10:00 AM</td>
+        <td>Web Design</td>
+        <td>LAB 1</td>
+        <td>Ms. Reyes</td>
+    </tr>
+
+    <tr>
+        <td>10:00 AM - 12:00 PM</td>
+        <td>Software Engineering</td>
+        <td>ROOM 205</td>
+        <td>Mr. Santos</td>
+    </tr>
+
+    <tr>
+        <td>Friday</td>
+        <td>8:00 AM - 11:00 AM</td>
+        <td>Project Development</td>
+        <td colspan="2">LAB 4 — Group Activity</td>
+    </tr>
+</table>
